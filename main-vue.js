fb948cf85709af3b65bd94181f5b46a8bcc1fc40
cloudhuh <claudiu.huhulia@gmail.com>
2023-06-19 17:23:47 +0200

    gonext & goprev functions

--- main-vue.js
+++ main-vue.js
@@ -34,7 +34,12 @@ const app = createApp({
     },
 
     methods:{
-
+        goNext(){
+            this.currentIndex++;
+        },
+        goPrev(){
+            this.currentIndex--;
+        }
     } 
 
 })
